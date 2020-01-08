fd8ec84b6580f8d70c6b8fb960c9f24b77f8e4bf
Preston Culbertson <pculbertson@stanford.edu>
2020-01-08 14:55:13 -0800

    minor controller bug

--- script/controller.py
+++ script/controller.py
@@ -37,7 +37,7 @@ class AdaptiveController():
         self.L = rospy.get_param('/ac/L')*np.eye(3)
         self.Kd_lin = rospy.get_param('/ac/Kd_lin')
         self.Kd_ang = rospy.get_param('/ac/Kd_ang')
-        self.Kd = np.diag(self.Kd_lin,self.Kd_lin,self.Kd_ang)
+        self.Kd = np.diag([self.Kd_lin,self.Kd_lin,self.Kd_ang])
         self.Gamma = rospy.get_param('/ac/Gamma')*np.eye(10)
         self.pos_elems = [0,1,4,7] #flags which elements to project to >0
         self.deadband = rospy.get_param('/ac/deadband')
