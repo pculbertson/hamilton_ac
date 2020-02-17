ad974e3940a80e3c0b855897ff3abca4d98fe79b
Preston Culbertson <pculbertson@stanford.edu>
2020-02-17 15:09:25 -0800

    lame bug

--- script/controller2.py
+++ script/controller2.py
@@ -144,8 +144,7 @@ class AdaptiveController():
                         [self.o_pos_elems,self.g_pos_elems,self.d_pos_elems,
                         self.c_pos_elems]):
                         if elems is not None:
-                            print(elems)
-                            param[elems] = np.max(param[elems],0.)
+                            param[elems] = np.maximum(param[elems],0.)
 
 
                 err_msg = Reference(Vector3(*q_err),Vector3(*dq_err),
